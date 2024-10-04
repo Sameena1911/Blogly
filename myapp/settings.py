2d8d6d9ba72969b446a21a1d7fa2d705475cf0ee
Sameena Mumtaz <sameenaferoz8@gmail.com>
2024-10-04 21:17:10 +0530

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-chbtyy5=-%tt-v!trkm=7q4ib7pomk86&+hz7@q!3#vs3q87m-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://blogly-0jcn.onrender.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
     "/blogly/static/"
 ]
