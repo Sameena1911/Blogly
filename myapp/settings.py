53711f45c11efdc3288ed775adc31171492ab621
Sameena Mumtaz <sameenaferoz8@gmail.com>
2024-10-30 15:08:26 +0530

    final Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogly'
+    'blogly',
 ]
 
 MIDDLEWARE = [
@@ -122,9 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS=[
-    "/blogly/static/"
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "blogly", "static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
